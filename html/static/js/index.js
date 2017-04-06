fad62c07e47cef1c0655ffae0ef5d3a1f684b454
Isaac <isaac@nicofun.club>
2017-04-07 04:33:28 +0900

    Add comments

--- html/static/js/index.js
+++ html/static/js/index.js
@@ -6,17 +6,47 @@ if(canvas.getContext) {
   alert('君はモダンブラウザを使うフレンズじゃないんだね....');
 }
 
-var alice = {
-  msg: 'hello',
-  x: 300,
-  y: 100
-};
+var canvasWidth = canvas.clientWidth;
+var canvasHeight = canvas.clientHeight;
+var lineHeight = 30;
+var comments = [];
+var comment = document.getElementById('comment');
+var tweet = document.getElementById('tweet');
+tweet.addEventListener('click', (event) => {
+  comments.push({
+    msg: comment.value,
+    x: canvasWidth,
+    y: commentY()
+  });
+  comment.value = '';
+});
+
+/*
+ * そのコメントのY座標を決定する
+ * @return {number} 決定されたY座標を返す
+ */
+function commentY() {
+  let count = 0;
+  comments.forEach((val, index) => {
+    if(val.x + ctx.measureText(val.msg).width > canvasWidth) {
+      count++;
+    }
+  });
+  console.log(count);
+  return lineHeight * (count + 1);
+}
 
 function draw() {
   ctx.clearRect(0, 0, 300, 150);
   ctx.font = '30px serif';
-  ctx.fillText(alice.msg, alice.x, alice.y);
-  alice.x -= 1;
+  comments.forEach((val, index) => {
+    ctx.fillText(val.msg, val.x, val.y);
+    val.x -= 1;
+    if(val.x + ctx.measureText(val.msg).width < 0) {
+      console.log("消すよ");
+      comments.splice(index, 1);
+    }
+  });
 
   window.requestAnimationFrame(draw);
 }
